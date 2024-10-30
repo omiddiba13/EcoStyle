81de6a3a855869bc9d7ece92588a3df1a70f4832
omid <omiddiba13@gmail.com>
2024-10-30 18:54:42 +0330

    fix

--- jsComPages/jsComponent/load.js
+++ jsComPages/jsComponent/load.js
@@ -1,6 +1,10 @@
 function loadShow() {
-	document.getElementById("loader").style.display = "block";
+	const loader = document.getElementById("loader");
+	loader.style.display = "flex"; // نمایش به صورت فلکس
+	loader.style.justifyContent = "center"; // مرکز‌چین کردن محتوا
+	loader.style.alignItems = "center"; // مرکز‌چین کردن عمودی محتوا
 }
+
 function loadHide() {
 	document.getElementById("loader").style.display = "none";
 }
